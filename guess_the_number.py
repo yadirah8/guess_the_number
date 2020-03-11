bad2f54334cc003827d68d1311a567fd575cab65
yadirah8 <yadinwandu4@gmail.com>
2020-03-11 12:59:52 +0100

    guess_the_number.py

---
+++ guess_the_number.py
@@ -0,0 +1,16 @@
+from random import randint
+
+
+def den():
+    num = number()
+    random_num = randint(0, 20)
+    print(ran)
+    return "too high" if random_num < num else "too low"
+
+
+def number():
+    y = int(input("Enter a random number"))
+    return y
+
+
+print(den())
